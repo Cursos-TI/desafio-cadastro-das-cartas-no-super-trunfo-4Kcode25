13891305440a91a0567543d9e2fef656913a3457
4Kcode25 <joao.victorsales2525@gmail.com>
2025-03-25 05:34:15 +0000

    Criação dos vetores

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,20 +4,32 @@
 // Declaração dos tributos da primeira carta
 
 int main() {
-    int população1;
+    int populacao1;
     float area1;
     float pib1;
-    int ponto t1;
+    int ponto_t1;
 
 //Declaração dos tributos da segunda carta
 
-    int população2;
+    int populacao2;
     float area2;
     float pib2;
-    int ponto t2;
+    int ponto_t2;
 
-    printf("Digite o numero da população: \n");
-    scanf("%d")
+    // Cadastro da primeira carta
+    printf("Coloque dados da primeira carta (ex A01): \n");
+    printf("Digite o numero da populacao: ");
+      scanf("%d", &populacao1);
+
+    printf("Coloque a area (em km): ");
+      scanf("%f", &area1);
+
+    printf("Coloque o valor do pib: ");
+      scanf("%f", &pib1);
+
+    printf("Coloque o valor dos pontos turisticos: ");
+      scanf("%d", ponto_t1);
 
     return 0;
+
 }
