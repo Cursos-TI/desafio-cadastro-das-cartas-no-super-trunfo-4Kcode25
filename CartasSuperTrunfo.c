63f13a4ee17639c488d2a8fe9dc2c042d93d6160
4Kcode25 <joao.victorsales2525@gmail.com>
2025-03-25 05:28:54 +0000

    criando printf 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
 
+// Declaração dos tributos da primeira carta
 
 int main() {
     int população1;
-    float area;
-    float pib;
-    int ponto t;
+    float area1;
+    float pib1;
+    int ponto t1;
 
-    
+//Declaração dos tributos da segunda carta
+
+    int população2;
+    float area2;
+    float pib2;
+    int ponto t2;
+
+    printf("Digite o numero da população: \n");
+    scanf("%d")
 
     return 0;
 }
