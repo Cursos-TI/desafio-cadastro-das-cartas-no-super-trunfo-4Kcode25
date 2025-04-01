576a48003b2f409e86c08f8e3175b474c6a0a3e4
4Kcode25 <joao.victorsales2525@gmail.com>
2025-04-01 03:04:43 +0000

    Corregindo

---
+++ Desafio: nível novato.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+
+// Declaração dos tributos da primeira carta
+
+int main() {
+    int populacao1;
+    float area1;
+    float pib1;
+    int ponto_t1;
+    
+//Declaração dos tributos da segunda carta
+
+    int populacao2;
+    float area2;
+    float pib2;
+    int ponto_t2;
+    
+              
+
+    // Cadastro da primeira carta
+    printf("Coloque dados da primeira carta (ex A01): \n");
+    printf("Digite o numero da populacao: ");
+      scanf("%d", &populacao1);
+
+    printf("Coloque a area (em km): ");
+      scanf("%f", &area1);
+
+    printf("Coloque o valor do pib: ");
+      scanf("%f", &pib1);
+
+    printf("Coloque o valor dos pontos turisticos: ");
+      scanf("%d", &ponto_t1);
+
+      // Cadastro da segunda carta
+      
+      printf("Coloque dados da segunda carta (ex A02): \n");
+      printf("Digite o numero da populacao: ");
+        scanf("%d", &populacao2);
+  
+      printf("Coloque a area (em km): ");
+        scanf("%f", &area2);
+  
+      printf("Coloque o valor do pib: ");
+        scanf("%f", &pib2);
+  
+      printf("Coloque o valor dos pontos turisticos: ");
+        scanf("%d", &ponto_t2);
+
+       
+
+        // Exibição dos dados da primeira carta
+     printf("\nDados da primeira carta (A01):\n");
+     printf("Populacao: %d habitantes\n", populacao1);
+     printf("Area: %.2f km²\n", area1);
+     printf("PIB: %.2f bilhoes\n", pib1);
+     printf("Pontos turisticos: %d\n", ponto_t1);
+     
+     // Exibição dos dados da segunda carta
+    printf("\nDados da segunda carta (B02):\n");
+    printf("Populacao: %d habitantes\n", populacao2);
+    printf("Area: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhoes\n", pib2);
+    printf("Pontos turisticos: %d\n", ponto_t2);
+    
+
+    return 0;
+
+}
